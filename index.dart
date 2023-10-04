5ba68ff6f1171591a383d60da42ae8805e8e3ee6
caristiz <catalinaaristizabal312613@correo.itm.edu.co>
2023-10-03 22:36:35 -0500

    Adding previous code

---
+++ index.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+void main () { 
+  print ("Hola");
+  print ("Escribe tu nombre");
+  String? nameInput = stdin.readLineSync();
+  print ("Un gusto conocerte $nameInput");
+  print ("Escribe tu fecha de nacimiento en formato dd/mm/yyyy");
+  String? birthdayInput = stdin.readLineSync();
+  DateTime? dateConverted =_convertTuDateTime(birthdayInput!);
+   if (dateConverted == null) {
+    print("la fecha de nacimiento es inválida, nos esviaste: $birthdayInput");
+    return;
+   }
+
+   var person = Person (
+    name: nameInput,
+    birthday: dateConverted,
+    age: DateTime.now().year - dateConverted.year,
+   );
+
+
+   print ("Tu nombre: ${person.name}");
+   print ("Tu Fecha de nacimiento: ${person.birthday}");
+   print ("¡Tu edad es: ${person.age} años!");
+}
+
+DateTime? _convertTuDateTime(String date) {
+  List<String> parts = date.split('/');
+  if (parts.length !=3) return null;
+  return DateTime.tryParse('${parts[2]}-${parts[1]}-${parts[0]}');
+}
+
+class Person {
+  final String? name;
+  final DateTime? birthday;
+  final int? age;
+  Person ( {this.name, this.birthday,this.age});
+
+}
